0c58bf43ee744bdec576467d34fb1192143fc306
Neil <Neil10241126@gmail.com>
2024-02-18 14:28:43 +0800

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -3,19 +3,41 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: () => import('../views/HomeView.vue'),
+    component: () => import('../views/FrontLayout.vue'),
+    children: [
+      {
+        path: '',
+        component: () => import('../views/front/HomeView.vue'),
+      },
+      {
+        path: 'products',
+        component: () => import('../views/front/ProductsView.vue'),
+      },
+      {
+        path: 'carts',
+        component: () => import('../views/front/CartsView.vue'),
+      },
+      {
+        path: 'login',
+        component: () => import('../views/front/LoginView.vue'),
+      },
+    ],
   },
   {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/AboutView.vue'),
+    path: '/admin',
+    component: () => import('../views/DashboardView.vue'),
+    children: [
+      {
+        path: 'products',
+        component: () => import('../views/dashboard/ProductsView.vue'),
+      },
+    ],
   },
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  linkActiveClass: 'active',
+  linkExactActiveClass: 'active',
   routes,
 });
 
